84bb794a744c6ac5c8c11f60fd351e242df81b34
Richard Decal <public@richarddecal.com>
2024-02-14 16:17:54 -0500

    Add git utilities for current commit and branch
    Signed-off-by: Richard Decal <public@richarddecal.com>

---
+++ tests/io/test_git.py
@@ -0,0 +1,21 @@
+from rd_utils.io import git_
+
+
+def mock_git(mocker):
+    git_ls_mock = mocker.patch.object(git_.git, "cmd")
+    git_ls_mock.Git().ls_remote.return_value = "\n".join(
+        [f"cmt_{i*1234567890}\trefs/heads/branch_{i}" for i in range(1, 10)]
+    )
+    return git_ls_mock
+
+
+def test_get_current_commit():
+    commit = git_.get_current_local_commit()
+    assert len(commit) == 12
+
+    commit = git_.get_current_local_commit(length=15)
+    assert len(commit) == 15
+
+
+def test_get_current_branch():
+    git_.get_current_local_branch()
