77461c60e7174c05319f5951db2ff99d8694b884
Richard Decal <public@richarddecal.com>
2024-02-14 16:49:45 -0500

    Add utility functions for string manipulation and data processing***
    ***Add test for deadline decorator
    
    Signed-off-by: Richard Decal <public@richarddecal.com>

---
+++ tests/io/test_context.py
@@ -0,0 +1,24 @@
+import time
+
+import pytest
+
+from rd_utils.io.context import DeadlineExceededException, deadline
+
+
+def test_deadline():
+    @deadline(1e-2)  # 1e-3 flakey with Bitbucket CI bc of their slow machines
+    def my_function():
+        time.sleep(1e-5)
+        return True
+
+    # This should not raise an exception
+    assert my_function() is True
+
+    @deadline(1e-4)
+    def my_function():
+        time.sleep(1e-3)
+        return True
+
+    # This should raise a DeadlineExceededException
+    with pytest.raises(DeadlineExceededException):
+        my_function()
