94015fade131772cd07f7a803b67e05480e00d00
Richard Decal <public@richarddecal.com>
2024-02-14 15:48:53 -0500

    Add rd_utils/conf.py and tests/test_conf.py files with default and overridden settings
    Signed-off-by: Richard Decal <public@richarddecal.com>

---
+++ rd_utils/conf.py
@@ -0,0 +1,39 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Container for default settings.
+
+    Override specific keys using environment variables.
+
+    Usage:
+    ```
+    from .conf import settings
+
+    print(settings.AWS_REGION)
+    ```
+    """
+
+    AWS_REGION: str = "us-east-1"
+    """
+    APP.
+    """
+    APP_BASEURL: str = Field(pattern=r"https://.+/$", default="https://app.mysite.io/")
+    ML_BUCKET: str = "ml-bucket-example"
+    """
+    EVENT BUS.
+    """
+    AWS_EVENT_BUS_REGION: str = "us-east-1"
+    AWS_EVENT_BUS_ARN: str = "arn:aws:events:us-east-1:123:event-bus/my-event-bus"
+    AWS_EVENT_BUS_SOURCE: str = "ml.richard"
+    """
+    SLACK ALERTS.
+    """
+    AWS_SNS_REGION: str = "us-east-1"
+    SNS_TOPIC_ARN: str = "arn:aws:sns:us-east-1:123:Slacker"
+    DEFAULT_SLACK_CHANNEL: str = "ml-microservice-errors"
+
+
+settings = Settings()
---
+++ tests/test_conf.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+from rd_utils.conf import Settings
+
+
+def test_default_settings():
+    settings = Settings()
+    assert (
+        settings.AWS_EVENT_BUS_ARN
+        == "arn:aws:events:us-east-1:123:event-bus/my-event-bus"
+    )
+
+
+def test_override_settings_with_env_vars():
+    new_region = "us-west-2"
+    with patch.dict("os.environ", {"AWS_REGION": new_region}):
+        settings = Settings()
+        assert settings.AWS_REGION == new_region
