41aec6bc06f5cb2ecc6cabdd75776b3d456ce8dd
Richard Decal <public@richarddecal.com>
2024-02-15 10:59:10 -0500

    Add mflow module with artifacts, cloudwatch_logs, flows, lineage, runs, and tasks submodules
    Signed-off-by: Richard Decal <public@richarddecal.com>

---
+++ rd_utils/io/mflow/__init__.py
@@ -0,0 +1,7 @@
+# settings so that we can run `from rd_utils.io.mflow import *` in a notebook
+from rd_utils.io.mflow.artifacts import *  # noqa: F401, F403
+from rd_utils.io.mflow.cloudwatch_logs import *  # noqa: F401, F403
+from rd_utils.io.mflow.flows import *  # noqa: F401, F403
+from rd_utils.io.mflow.lineage import *  # noqa: F401, F403
+from rd_utils.io.mflow.runs import *  # noqa: F401, F403
+from rd_utils.io.mflow.tasks import *  # noqa: F401, F403
