a22e6b3fd5fdd3c4342a3676c8fd4ecf3f6f51a0
Richard Decal <public@richarddecal.com>
2024-02-15 10:47:17 -0500

    Add test for sending a plot to Slack
    Signed-off-by: Richard Decal <public@richarddecal.com>

---
+++ tests/io/slack/test_send_message.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pytest
+
+from rd_utils.io.slack import BOT_TEST_CHANNEL, Slacker
+
+
+@pytest.mark.skip(
+    reason="run manually only since this actually sends requests to slack"
+)
+def test_send_plot():
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax1.plot(range(10), "b-")
+
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.plot(range(20), "r^")
+
+    bot = Slacker()
+    bot.send_file(fig, BOT_TEST_CHANNEL, "a plot with a caption")
