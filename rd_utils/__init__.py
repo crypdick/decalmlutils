640d743b2bf6cb3f56808a8faf8c5c11090bea54
Richard Decal <public@richarddecal.com>
2024-05-04 06:10:41 -0400

    rename logging.py to prevent name collision
    Signed-off-by: Richard Decal <public@richarddecal.com>

--- rd_utils/__init__.py
+++ rd_utils/__init__.py
@@ -2,7 +2,7 @@
 # make sure warnings are imported
 import warnings
 
-from rd_utils.logging import setup_default_logging
+from rd_utils.logging_utils import setup_default_logging
 
 # always show deprecation warnings
 warnings.simplefilter("always", DeprecationWarning)
