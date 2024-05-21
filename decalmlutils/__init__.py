9afbda4514f9f0f7b881b46f779d591264c1183f
Richard Decal <public@richarddecal.com>
2024-05-21 17:16:08 -0400

    make misc utils easier to import
    Signed-off-by: Richard Decal <public@richarddecal.com>

--- decalmlutils/__init__.py
+++ decalmlutils/__init__.py
@@ -2,6 +2,7 @@
 # make sure warnings are imported
 import warnings
 
+from decalmlutils.io.misc import *  # noqa: F401, F403
 from decalmlutils.logging_utils import setup_default_logging
 
 # always show deprecation warnings
