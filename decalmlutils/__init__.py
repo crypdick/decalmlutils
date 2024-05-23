271d86fa2600c7500eb33330fc0ebbba515c4f97
Richard Decal <public@richarddecal.com>
2024-05-23 13:07:14 -0400

    disable logging setup
    Signed-off-by: Richard Decal <public@richarddecal.com>

--- decalmlutils/__init__.py
+++ decalmlutils/__init__.py
@@ -2,7 +2,6 @@
 # make sure warnings are imported
 import warnings
 
-from .logging_utils import setup_default_logging
 from .misc import *  # noqa: F401, F403
 
 # always show deprecation warnings
@@ -10,6 +9,3 @@ warnings.simplefilter("always", DeprecationWarning)
 
 
 __version__ = "0.0.9"
-
-
-setup_default_logging()
