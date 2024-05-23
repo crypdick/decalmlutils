b64154f105cc75ab521ee81c628a9dca65e8167b
Richard Decal <public@richarddecal.com>
2024-05-23 13:59:54 -0400

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ extras = {
 
 setup(
     name="decalmlutils",
-    version="0.0.11",
+    version="0.0.12",
     author="Richard Decal",
     author_email="public@richarddecal.com",
     description="Useful functions when working with Machine Learning in Python",
